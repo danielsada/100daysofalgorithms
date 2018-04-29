0d6916d22f0f409fc465ee2bf41269c04b3f46ef
Daniel Sada Caraveo <danielsadac@gmail.com>
2018-04-28 22:04:17 -0500

    Day 76. Flow network.

---
+++ algorithms/min-flow/flownetwork.py
@@ -0,0 +1,36 @@
+
+__author__ = "Daniel Sada"
+__license__ = "MIT Licence"
+__email__ = "yo@danielsada.mx"
+
+
+class FlowNetWork:
+    """"
+    Describes a edge that has a capacity and a flow.
+    """
+
+    def __init__(self, v):
+        self.V = v
+        self.E = 0
+        for i in range(0, self.V):
+            self.adj[i] = []
+
+    def __repr__(self):
+        f = "FlowNetwork: \n"
+        for i in range(self.V):
+            f += f"Nodes From {i}: \n"
+            for e in self.adj[i]:
+                f += e.__repr__()
+        return f
+
+    def addEdge(self, e):
+        v = e.source()
+        w = e.destination()
+        self.adj[v].append(w)
+        self.adj[w].append(v)
+        self.E += 1
+    
+    def edges(self):
+        for i in range(self.V):
+            for e in self.adj[v]:
+                yield e
